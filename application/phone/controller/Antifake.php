a20addc91457e953db53a9fb47a017d4117793b8
VinceLim68 <1500725438@qq.com>
2018-01-11 10:36:57 +0800

    防伪增加时间截，增加查询记录的登记

--- application/phone/controller/Antifake.php
+++ application/phone/controller/Antifake.php
@@ -2,18 +2,26 @@
 namespace app\phone\controller;
 use think\Controller;
 use app\phone\model\CPGRecordModel;
+use app\evalu\logic\LoginLogic;
+use app\phone\model\AntiModel;
 
 class Antifake extends Controller {
     
     public function index() {
         //手机查询防伪
         $reportid = input('id');
-//         halt($reportid);
-        $dbDB = new CPGRecordModel();
-        $resu = $dbDB->field('customer,RAddress,RMoney,ZID')->where('ZID',$reportid)->find();
-//          $resu = $dbDB->where('ZID',$reportid)->find(); 
-//          halt($resu);
+        $ip = LoginLogic::getIP();
+//         $dbDB = ;
+        $resu = (new CPGRecordModel())->field('customer,RAddress,RMoney,ZID')->where('ZID',$reportid)->find();
+//         dump($resu);
+        $anti = new AntiModel();
+        $anti->data([
+            'ip'    =>  $ip,
+            'zid'   =>  $resu['ZID']
+        ])->save();
+//         dump($anti->create_time);
         $this->assign('res',$resu);
+        $this->assign('time',$anti->create_time);
         return $this->fetch();
     }
     
