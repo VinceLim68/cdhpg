7d5c92934d9128a4e39827e36618c491eccf982e
VinceLim68 <1500725438@qq.com>
2019-08-27 09:36:40 +0800

    2019082705

--- application/evalu/controller/Common.php
+++ application/evalu/controller/Common.php
@@ -77,41 +77,6 @@ class Common extends Controller
  		if($isPhone=='非手机'){
  		    //只有经过授权才能使用桌面系统
     		if(!$auth->check('onDesktop', session('user.user_id'))){
-    		    // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
-    		    if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
-    		        dump($_SERVER['HTTP_X_WAP_PROFILE']);
-    		        halt('HTTP_X_WAP_PROFILE');
-    		    }
-    		    //       echo '1';
-    		    // 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
-    		    if (isset($_SERVER['HTTP_VIA'])) {
-    		        // 找不到为flase,否则为true
-    		        dump($_SERVER['HTTP_VIA']);
-    		        halt('HTTP_VIA');
-    		    }
-    		    //       echo '2';
-    		    // 脑残法，判断手机发送的客户端标志,兼容性有待提高。其中'MicroMessenger'是电脑微信
-    		    if (isset($_SERVER['HTTP_USER_AGENT'])) {
-    		        $clientkeywords = array('nokia','sony','ericsson','mot','samsung','htc','sgh','lg','sharp','sie-','philips','panasonic','alcatel','lenovo','iphone','ipod','blackberry','meizu','android','netfront','symbian','ucweb','windowsce','palm','operamini','operamobi','openwave','nexusone','cldc','midp','wap','mobile','MicroMessenger');
-    		        // 从HTTP_USER_AGENT中查找手机浏览器的关键字
-    		        if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
-    		            dump($_SERVER['HTTP_USER_AGENT']);
-    		            halt('HTTP_USER_AGENT');
-    		        }
-    		    }
-    		    //       echo '3';
-    		    // 协议法，因为有可能不准确，放到最后判断
-    		    if (isset ($_SERVER['HTTP_ACCEPT'])) {
-    		        // 如果只支持wml并且不支持html那一定是移动设备
-    		        // 如果支持wml和html但是wml在html之前则是移动设备
-    		        if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
-    		            dump($_SERVER['HTTP_ACCEPT']);
-    		            halt('HTTP_ACCEPT');
-    		        }
-    		    }
-    		    
-    		    halt($isPhone);
-    		    
     		    
     		    $this->error('程序出错了！！！如需要合作开发或者业务联系，请找18006006153林先生！！');
     		}
